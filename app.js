32de10732e6d66fb1eebd018c373bd1c35753126
Ajax <alexjameschu@gmail.com>
2026-07-06 15:56:54 -0700

    Remove and update image URLs in app.js

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var images = [
   "https://www.chromethemer.com/wallpapers/chromebook-wallpapers/images/960/water-pokemon-chromebook-wallpaper.jpg",
   "https://i.pinimg.com/736x/07/bd/cb/07bdcb605727348d60ac19d4e8215e06.jpg",
   "https://www.chromethemer.com/download/hd-wallpapers/pokemon-3840x2160.jpg",
-  "https://images5.alphacoders.com/109/thumb-1920-1092839.jpg",
-  "https://pbs.twimg.com/media/GYbQ2oBaAAAwskB.jpg",
-  "https://i.pinimg.com/736x/0c/43/6d/0c436df5549f92d21cfcad66387d156e.jpg",
-  "https://wallpapercave.com/wp/wp10135847.png"
+  "https://pbs.twimg.com/media/GYbQ2oBaAAAwskB.jpg"
 ];
 
 var randomIndex = Math.floor(Math.random() * images.length);
