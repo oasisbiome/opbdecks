d2b4a54ad2694dd6c69ccd5dc078a58a89532ae8
Ajax <alexjameschu@gmail.com>
2026-07-06 15:12:20 -0700

    Add random background image and copy functions

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-body {
-  margin: 20px;
+// This running block picks a random background image every time the page loads
+var images = [
+  "https://images5.alphacoders.com/109/thumb-1920-1092839.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdFFveGe9D9T8dfdR17hoKHrSRCmkLVpoLUSt69NlXBg&s=10",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMA5L2b-9B9I7nmvZYaKH_dLj_S9Czm6Gs6nY5vJBPoQ&s=10",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7YuFejivtTir2CJu-MdlGhSyQwtDlN-kKL8aIuACqtQ&s=10",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcDUH4XaCq037vjr2VsfDGz6AjVzgToLhGtHwdgLSacA&s=10",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX5CmfWoBT_Ur20Eedz7kB_RiKnDreeASmZVA8-cjw&s=10",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbZ_0qThPYxWw0ptnyoAFrCHzAMGW5ErWFpWV4fYWb7g&s=10"
+];
+
+var randomIndex = Math.floor(Math.random() * images.length);
+document.body.style.backgroundImage = "url('" + images[randomIndex] + "')";
+
+
+// These are our basic copy button functions
+function copyDeck1() {
+  var listText = document.getElementById("myList1").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
 }
 
-h1 {
-  text-shadow: 2px 2px black;
+function copyDeck2() {
+  var listText = document.getElementById("myList2").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
 }
 
-h2 {
-  font-size: 20px;
+function copyDeck3() {
+  var listText = document.getElementById("myList3").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
 }
 
-pre {
-  background-color: #141522;
-  color: #F0F2F5;
-  padding: 15px;
-  border: 2px solid grey;
+function copyDeck4() {
+  var listText = document.getElementById("myList4").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
 }
 
-button {
-  background-color: #1E202E;
-  color: #FFCB05;
-  border: 2px solid #FFCB05;
-  padding: 10px;
-  font-weight: bold;
-  font-size: 16px;
-}
-
-button:hover {
-  background-color: #FFCB05;
-  color: black;
+function copyDeck5() {
+  var listText = document.getElementById("myList5").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
 }
