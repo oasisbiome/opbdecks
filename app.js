f8a5defce99903f9ce23fad4e38f9e1015a39b5b
Ajax <alexjameschu@gmail.com>
2026-07-07 12:51:58 -0700

    Add functions to copy decklists to clipboard

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+function copyDeck1() {
+  var listText = document.getElementById("myList1").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck2() {
+  var listText = document.getElementById("myList2").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck3() {
+  var listText = document.getElementById("myList3").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck4() {
+  var listText = document.getElementById("myList4").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck5() {
+  var listText = document.getElementById("myList5").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck6() {
+  var listText = document.getElementById("myList6").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck7() {
+  var listText = document.getElementById("myList7").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck8() {
+  var listText = document.getElementById("myList8").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
+function copyDeck9() {
+  var listText = document.getElementById("myList9").innerText;
+  navigator.clipboard.writeText(listText);
+  alert("Decklist copied to clipboard.");
+}
+
 function sortMyDecks() {
   var value = document.getElementById("deckSorter").value;
   var container = document.getElementById("deckContainer");
@@ -27,5 +81,22 @@ function sortMyDecks() {
 
   for (var i = 0; i < boxes.length; i++) {
     container.appendChild(boxes[i]);
+  }
+}
+
+function searchMyDecks() {
+  var input = document.getElementById("deckSearch").value.toLowerCase();
+  var container = document.getElementById("deckContainer");
+  var boxes = container.getElementsByClassName("deck-box");
+
+  for (var i = 0; i < boxes.length; i++) {
+    var name = boxes[i].getAttribute("data-name").toLowerCase();
+    var type = boxes[i].getAttribute("data-type").toLowerCase();
+    
+    if (name.indexOf(input) > -1 || type.indexOf(input) > -1) {
+      boxes[i].style.display = "";
+    } else {
+      boxes[i].style.display = "none";
+    }
   }
 }
